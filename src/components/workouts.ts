6e9d3fadfecd2caf3d8483a063ee5f5bbd1e7e8a
João Centeno <Joao.Centeno@pt.bosch.com>
2018-09-11 08:17:48 +0100

    Create workouts metods to get data

--- src/components/workouts.ts
+++ src/components/workouts.ts
@@ -1,7 +1,47 @@
-export default class Workouts {
+export default class Wods {
 
-    // types: string[];
-    // difficulty: string[];
-    // categories: string[];
-    // equipments: string[];
+    private data: any;
+
+    constructor(source: string) {
+        this.data = JSON.parse(source);
+    }
+
+    getRandom() {
+        const randomId:number = Math.floor(Math.random() * this.data.workouts.length);
+        return this.data.workouts[randomId];
+    }
+
+    getById(id: number) {
+        if (id === 0 || id > this.data.workouts.length) return {};
+
+        return this.data.workouts.find((w) => id === w.id);
+    }
+
+    getByName(name: string) {
+        if (name === '') return {};
+
+        return this.data.workouts.find((w) => name === w.name);
+    }
+
+    getByType(type: number) {
+        if (type >= this.data.types.length) return [];
+
+        return this.data.workouts.filter((w) => type === w.type);
+    }
+
+    getByDifficulty(difficulty: number) {
+        if (difficulty >= this.data.types.length) return [];
+
+        return this.data.workouts.filter((w) => difficulty === w.difficulty);
+    }
+
+    getByCategory(cat: number) {
+        if (cat >= this.data.types.length) return [];
+
+        return this.data.workouts.filter((w) => cat === w.category);
+    }
+
+    getAll() {
+        return this.data.workouts;
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
 import Controler from './components/controller';
-import Workouts from './components/workouts';
+import Wods from './components/workouts';
 
 class App {
     private controller = new Controler();
-    private workouts: Workouts;
-    private _data: Workouts;
+    private wods;
 
     constructor() {
         if (typeof localStorage['data-workouts'] === 'undefined') {
             this.controller.loadData(this.handleDataRequest.bind(this));
         }
         else {
-            this._data = JSON.parse(localStorage['data-workouts']);
-            console.log(this._data);
+            const data = localStorage['data-workouts'];
+            this.wods = new Wods(data);
+            console.log(this.wods.getAll());
         }    
         
-        console.log(this._data);
-        
+        //console.log(this._data);       
+        //this.workouts.get();
     }
 
     private handleDataRequest(response: any) {
         localStorage.setItem('data-workouts', response);
-        this._data = JSON.parse(response);
+        //this._data = JSON.parse(response);
     }
 }
 
